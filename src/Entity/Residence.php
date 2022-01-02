837de799a8305808cb0fbb49647afcf4ba136bf4
Yuki60 <merciert60@gmail.com>
2022-01-02 21:39:45 +0100

    reset password

---
+++ src/Entity/Residence.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResidenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ResidenceRepository::class)
+ */
+class Residence
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $zip_code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $inventory_file;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $relation;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(string $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getInventoryFile(): ?string
+    {
+        return $this->inventory_file;
+    }
+
+    public function setInventoryFile(string $inventory_file): self
+    {
+        $this->inventory_file = $inventory_file;
+
+        return $this;
+    }
+
+    public function getRelation(): ?User
+    {
+        return $this->relation;
+    }
+
+    public function setRelation(?User $relation): self
+    {
+        $this->relation = $relation;
+
+        return $this;
+    }
+
+}
