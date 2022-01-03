2be5488fb07406d9ce10faf101ba903caf1e2dfc
Yuki60 <merciert60@gmail.com>
2022-01-03 16:33:54 +0100

    templates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,18 +11,7 @@ class DefaultController extends AbstractController
 
     public function index(Request $request): Response
     {
-        // Exercice : rediriger l'utilisateur sur la route "bonjour" en lui passant le paramètre "user"
-        $user = $request->query->get('user', 'world');
-
-        return $this->redirectToRoute('bonjour', [
-            'prenom' => $user,
-        ]);
+        return $this->render('default/index.html.twig',);
     }
 
-    public function bonjour(string $prenom = 'world'): Response
-    {
-        return $this->render('default/bonjour.html.twig', [
-            'prenom' => $prenom,
-        ]);
-    }
 }
