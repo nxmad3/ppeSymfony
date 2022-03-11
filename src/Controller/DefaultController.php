448ef94ea585d456aa80f88f21cad699df37b1e8
Yuki60 <merciert60@gmail.com>
2022-03-11 16:30:36 +0100

    design email

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
@@ -13,13 +15,21 @@ class DefaultController extends AbstractController
     public function index(Request $request): Response
     {
 
-        if($this->getUser()){
+        if ($this->getUser()) {
             $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
-            return $this->render('default/index.html.twig',[
-                'user'=>$user
+            return $this->render('default/index.html.twig', [
+                'user' => $user
             ]);
         }
         return $this->render('login/index.html.twig');
     }
 
+    #[Route('/deleteuser', name: 'deleteuser')]
+    public function deleteUserAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($this->getUser());
+        $em->flush();
+        return $this->render('login/index.html.twig');
+    }
 }
